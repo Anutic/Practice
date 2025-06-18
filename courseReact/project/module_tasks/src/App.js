bcdedfa65f07b36f18941079e9749b830abc212a
Anutic <megazvezba@gmail.com>
2025-06-18 10:59:45 +0300

    task 3, course React

--- courseReact/project/module_tasks/src/App.js
+++ courseReact/project/module_tasks/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import React from 'react';
 import Task1 from './tasks/Task1';
 import Task2 from './tasks/Task2';
+import Task3 from './tasks/Task3';
 
 function App() {
   return (
@@ -11,6 +12,8 @@ function App() {
       <Task1 />
       <hr style={{ margin: '40px 0' }} />
       <Task2 />
+      <hr style={{ margin: '48px 0' }} />
+      <Task3 />
     </div>
   );
 }
